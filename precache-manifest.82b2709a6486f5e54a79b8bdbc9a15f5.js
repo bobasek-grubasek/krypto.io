a0b12dc6b19ed4766cc6508e58ee65d9f625b657
przemek <someRandommail@mail.com>
2020-05-08 01:05:51 +0200

    Updates

---
+++ precache-manifest.82b2709a6486f5e54a79b8bdbc9a15f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d862d2cbe0d818c570bc1a0da6d5a29",
+    "url": "/krypto/index.html"
+  },
+  {
+    "revision": "c8a822f819f8e714441c",
+    "url": "/krypto/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c397afd22a85e26921d",
+    "url": "/krypto/static/js/2.eec57751.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krypto/static/js/2.eec57751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a822f819f8e714441c",
+    "url": "/krypto/static/js/main.a8d39fed.chunk.js"
+  },
+  {
+    "revision": "e831af58af2b9fa75c50",
+    "url": "/krypto/static/js/runtime-main.132d6f80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/krypto/static/media/logo.5d5d9eef.svg"
+  }
+]);
